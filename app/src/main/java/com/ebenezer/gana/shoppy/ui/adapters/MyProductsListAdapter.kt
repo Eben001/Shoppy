759e539b70957a5f888c0e4cb415329b44f8c073
Gana Ebenezer <ebenezergana@gmail.com>
2022-03-08 01:29:09 +0100

    added viewBinding to MyProductsListAdapter code clean up

--- app/src/main/java/com/ebenezer/gana/shoppy/ui/adapters/MyProductsListAdapter.kt
+++ app/src/main/java/com/ebenezer/gana/shoppy/ui/adapters/MyProductsListAdapter.kt
@@ -10,58 +10,65 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ebenezer.gana.shoppy.R
+import com.ebenezer.gana.shoppy.databinding.ListItemProductBinding
 import com.ebenezer.gana.shoppy.models.Products
 import com.ebenezer.gana.shoppy.ui.activities.ProductDetailsActivity
 import com.ebenezer.gana.shoppy.ui.fragments.ProductsFragment
 import com.ebenezer.gana.shoppy.utils.Constants
 import com.ebenezer.gana.shoppy.utils.GlideLoader
 
-open class MyProductsListAdapter(
+class MyProductsListAdapter(
     private val context: Context,
-    private var list:ArrayList<Products>,
+    private var list: ArrayList<Products>,
     private val fragment: ProductsFragment
-): RecyclerView.Adapter<RecyclerView.ViewHolder> (){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return MyViewHolder(
-            LayoutInflater.from(context).inflate(
-                R.layout.list_item_product,
-                parent,
-                false
+) : RecyclerView.Adapter<MyProductsListAdapter.ViewHolder>() {
+
+
+    inner class ViewHolder(val binding: ListItemProductBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        private lateinit var products: Products
+
+        fun bind(products: Products) {
+            this.products = products
+
+            GlideLoader(context).loadProductPicture(
+                products.image,
+                binding.ivItemImage
             )
-        )
-    }
+            binding.tvItemName.text = products.title
+            binding.tvItemPrice.text = "₦${products.price}"
+            binding.tvItemDescription.text =
+                products.description
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val model = list[position]
+            binding.ibDeleteProduct.setOnClickListener {
 
-        if (holder is MyViewHolder){
-            GlideLoader(context).loadProductPicture(model.image, holder.itemView.findViewById(R.id.iv_item_image))
-            holder.itemView.findViewById<TextView>(R.id.tv_item_name).text = model.title
-            holder.itemView.findViewById<TextView>(R.id.tv_item_price).text = "₦${model.price}"
-            holder.itemView.findViewById<TextView>(R.id.tv_item_description).text = model.description
-
-            holder.itemView.findViewById<ImageButton>(R.id.ib_delete_product).setOnClickListener{
-
-                fragment.deleteProduct(model.product_id)
+                fragment.deleteProduct(products.product_id)
             }
-            holder.itemView.setOnClickListener{
+            itemView.setOnClickListener {
                 val intent = Intent(context, ProductDetailsActivity::class.java)
-                intent.putExtra(Constants.EXTRA_PRODUCT_ID, model.product_id)
-                intent.putExtra(Constants.EXTRA_PRODUCT_OWNER_ID, model.user_id)
+                intent.putExtra(Constants.EXTRA_PRODUCT_ID, products.product_id)
+                intent.putExtra(Constants.EXTRA_PRODUCT_OWNER_ID, products.user_id)
 
                 context.startActivity(intent)
             }
 
-
-
         }
-    }
-    override fun getItemCount(): Int {
-        return list.size
 
     }
 
-    class MyViewHolder(itemView:View):RecyclerView.ViewHolder(itemView)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ListItemProductBinding.inflate(
+            LayoutInflater.from(context),
+            parent, false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount() = list.size
 
 
 }
